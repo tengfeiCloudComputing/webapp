e25e315f45d07f7334ee12da12ee9223ef8733e6
TengfeiWang <wang.ten@northeastern.edu>
2023-02-01 00:25:19 -0500

    validations and unit tests

--- webapp/src/test/java/com/tengfei/webapp/WebappApplicationTests.java
+++ webapp/src/test/java/com/tengfei/webapp/WebappApplicationTests.java
@@ -1,7 +1,10 @@
 package com.tengfei.webapp;
 
+import com.tengfei.webapp.controller.UserController;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class WebappApplicationTests {
@@ -10,4 +13,10 @@ class WebappApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	void testUsingSimpleRegex(){
+		String emailAddress = "username@domain.com";
+		String regexPattern = "^(.+)@(\\S+)$";
+		assertTrue(UserController.patternMatches(emailAddress, regexPattern));
+	}
 }
