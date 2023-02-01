dedb6bcdeab922508c3debd8712beff658a69264
TengfeiWang <wang.ten@northeastern.edu>
2023-01-31 22:15:18 -0500

    apis with basic auth

---
+++ webapp/src/main/java/com/tengfei/webapp/model/User.java
@@ -0,0 +1,92 @@
+package com.tengfei.webapp.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @Column(name ="first_name")
+    private String firstName;
+
+    @Column(name ="last_name")
+    private String lastName;
+
+    @Column(name ="password")
+    private String password;
+
+    @Column(name ="email",unique = true)
+    private String email;
+
+    @CreationTimestamp
+    private LocalDateTime account_created;
+
+    @UpdateTimestamp
+    private LocalDateTime account_updated;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDateTime getAccount_created() {
+        return account_created;
+    }
+
+    public void setAccount_created(LocalDateTime account_created) {
+        this.account_created = account_created;
+    }
+
+    public LocalDateTime getAccount_updated() {
+        return account_updated;
+    }
+
+    public void setAccount_updated(LocalDateTime account_updated) {
+        this.account_updated = account_updated;
+    }
+}
---
+++ webapp/src/main/java/com/tengfei/webapp/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.tengfei.webapp.repository;
+
+import com.tengfei.webapp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    public User findByEmail(String email);
+}
