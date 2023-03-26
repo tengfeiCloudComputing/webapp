c1c1be316779ac7d7253b6201473dcbfe3dd9abb
TengfeiWang <wang.ten@northeastern.edu>
2023-03-26 17:43:06 -0400

    update cloud watch logs and metrics

--- src/main/java/com/tengfei/webapp/controller/ImageController.java
+++ src/main/java/com/tengfei/webapp/controller/ImageController.java
@@ -14,6 +14,10 @@ import com.tengfei.webapp.model.User;
 import com.tengfei.webapp.repository.ImagePerository;
 import com.tengfei.webapp.repository.ProductRepository;
 import com.tengfei.webapp.repository.UserRepository;
+import com.timgroup.statsd.StatsDClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -29,6 +33,10 @@ import java.util.*;
 
 @RestController
 public class ImageController {
+    Logger logger = LoggerFactory.getLogger(ImageController.class);
+
+    @Autowired
+    private StatsDClient statsDClient;
     private final UserRepository userRepository;
     private final ProductRepository productRepository;
     private final ImagePerository imagePerository;
@@ -44,9 +52,11 @@ public class ImageController {
 
     @GetMapping("/v1/product/{product_id}/image")
     public ResponseEntity<List<Image>> getAllImage(@PathVariable int product_id){
-
+        statsDClient.incrementCounter("csye6225.http.get.all.images");
+        logger.info("getting all the image of a product");
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication == null){
+            logger.error("authentication failed");
             return ResponseEntity.status(401).build();
         }
         String username = authentication.getName();
@@ -58,18 +68,22 @@ public class ImageController {
         }
 
         if (!currentProduct.get().getUser().equals(loginUser)){
+            logger.error("the user has existed");
             return ResponseEntity.status(403).build();
         }
 
         Optional<List<Image>> imagesByProduct_id = imagePerository.findImagesByProduct_Id(product_id);
-
+        logger.info("getting all the image of a product successfully");
         return ResponseEntity.status(200).body(imagesByProduct_id.get());
     }
 
     @PostMapping("/v1/product/{product_id}/image")
     public ResponseEntity<Image> upLoadImage(@PathVariable int product_id, @RequestParam("file") MultipartFile file) throws IOException {
+        statsDClient.incrementCounter("csye6225.http.upload.an.image");
+        logger.info("uploading an image");
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication == null){
+            logger.error("post an image but user not authenticated");
             return ResponseEntity.status(401).build();
         }
         String username = authentication.getName();
@@ -78,10 +92,12 @@ public class ImageController {
         Optional<Product> curProduct = productRepository.findById(product_id);
 
         if (curProduct.isEmpty()){
+            logger.error("post an image but product is not found");
             return ResponseEntity.status(400).build();
         }
 
         if (!curProduct.get().getUser().equals(loginUser)){
+            logger.error("post an image for another user not allowed");
             return ResponseEntity.status(403).build();
         }
 
@@ -126,6 +142,7 @@ public class ImageController {
         }
 
         if (!isImage){
+            logger.error("post an image but not image format file");
             return ResponseEntity.status(400).build();
         }
 
@@ -141,7 +158,7 @@ public class ImageController {
             nImage.setS3_bucket_path(url);
             nImage.setDate_created(LocalDateTime.now());
             imagePerository.saveAndFlush(nImage);
-
+            logger.info("uploaded an image successfully");
             return ResponseEntity.status(HttpStatus.OK).body(nImage);
         }catch (Exception e){
             throw e;
@@ -150,50 +167,68 @@ public class ImageController {
 
     @GetMapping("/v1/product/{product_id}/image/{image_id}")
     public ResponseEntity<Image> getImageDetail(@PathVariable String image_id, @PathVariable String product_id){
+        statsDClient.incrementCounter("csye6225.http.get.an.image.detail");
+        logger.info("getting an image detail");
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication == null){
+            logger.error("get an image but user not authenticated");
+
             return ResponseEntity.status(401).build();
         }
         String username = authentication.getName();
         User loginUser = userRepository.findByUsername(username);
         Optional<Product> currentProduct= productRepository.findById(Integer.valueOf(product_id));
         if (currentProduct.isEmpty()){
+            logger.error("get an image but product not found");
+
             return ResponseEntity.status(404).build();
         }
 
 
         if (!currentProduct.get().getUser().equals(loginUser)){
+            logger.error("get an image of another user not allowed");
+
             return ResponseEntity.status(403).build();
         }
 
         Optional<Image> foundImage = imagePerository.findById(image_id);
 
         if (foundImage.isEmpty()){
+            logger.error("cannot find image");
             return ResponseEntity.status(404).build();
         }
+        logger.info("get an image detail successfully");
         return ResponseEntity.status(200).body(foundImage.get());
     }
 
     @DeleteMapping("/v1/product/{product_id}/image/{image_id}")
     public ResponseEntity<String> deleteImage(@PathVariable String image_id, @PathVariable String product_id){
+        statsDClient.incrementCounter("csye6225.http.delete.an.image");
+        logger.info("deleting an image");
+
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication == null){
+            logger.error("delete an image not authenticated");
             return ResponseEntity.status(401).build();
         }
         String username = authentication.getName();
         User loginUser = userRepository.findByUsername(username);
         Optional<Product> currentProduct= productRepository.findById(Integer.valueOf(product_id));
         if (currentProduct.isEmpty()){
+            logger.error("delete an image but product not found");
             return ResponseEntity.status(404).build();
         }
 
         if (!currentProduct.get().getUser().equals(loginUser)){
+            logger.error("delete another user's image not allowed");
             return ResponseEntity.status(403).build();
         }
 
         Optional<Image> foundImage = imagePerository.findById(image_id);
 
         if (foundImage.isEmpty()){
+            logger.error("delete an image but image not found");
+
             return ResponseEntity.status(404).build();
         }
         // Amazon find url in s3
@@ -215,10 +250,13 @@ public class ImageController {
         try{
             Optional<Image> fImage = imagePerository.findById(image_id);
             if (fImage.isEmpty()){
+                logger.error("delete an image but image not found");
                 return ResponseEntity.status(404).build();
             }
             amazonS3.deleteObject(bucketName,urls[urls.length-1]);
             imagePerository.delete(fImage.get());
+            logger.info("deleted an image detail successfully");
+
             return ResponseEntity.status(204).build();
         }catch (Exception e){
             throw e;
